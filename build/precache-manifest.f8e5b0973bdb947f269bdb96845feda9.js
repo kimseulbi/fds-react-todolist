b4f47312d6179c018a35a707a0d8a15852b0efa9
kimseulbi <ksb5166@gmail.com>
2018-11-18 01:53:25 +0900

    webpack 제거

---
+++ build/precache-manifest.f8e5b0973bdb947f269bdb96845feda9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc351278ab4929c66c3e",
+    "url": "/static/js/main.bc351278.chunk.js"
+  },
+  {
+    "revision": "fcdd4a9c3d38eaf2951a",
+    "url": "/static/js/1.fcdd4a9c.chunk.js"
+  },
+  {
+    "revision": "bc351278ab4929c66c3e",
+    "url": "/static/css/main.24d9bb72.chunk.css"
+  },
+  {
+    "revision": "75ab9fd1a45b36b74f0c941120019f0d",
+    "url": "/index.html"
+  }
+];
--- webpack/production.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const common = require('./common');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
-
-module.exports = merge(common, {
-  plugins: [
-    new MinifyPlugin()
-  ]
-});
